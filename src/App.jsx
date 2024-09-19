15f32d90ac70237646fb9f6a58e69d20455483aa
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-09-19 12:37:43 +0330

    Create "Home" route and "NotFound" route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Auth from "./Pages/Auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./Pages/CompleteProfile";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/auth" element={<Auth />} />
           <Route path="/complete-profile" element={<CompleteProfile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </QueryClientProvider>
