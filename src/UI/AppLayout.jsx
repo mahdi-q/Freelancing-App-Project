d2a91b9529d46ee7a4bca5b83e259aa96c3428dd
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-09-22 20:56:26 +0330

    Develope "ProjectTable" component with its related functions

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -10,7 +10,7 @@ function AppLayout() {
       <Sidebar />
 
       <div className="overflow-y-auto bg-secondary-100 p-8">
-        <div className="mx-auto max-w-screen-md">
+        <div className="mx-auto max-w-screen-lg">
           <Outlet />
         </div>
       </div>
---
+++ src/UI/Empty.jsx
@@ -0,0 +1,6 @@
+function Empty({ resourceName }) {
+  return (
+    <p className="font-bold text-secondary-700">هیچ {resourceName} یافت نشد !</p>
+  );
+}
+export default Empty;
