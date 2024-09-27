532b141a3d67b54d341db4d124ac30e371671257
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-09-27 17:39:39 +0330

    Develope "Modal" component

--- src/Features/Projects/ProjectRow.jsx
+++ src/Features/Projects/ProjectRow.jsx
@@ -1,9 +1,16 @@
+import { TbPencilMinus } from "react-icons/tb";
 import Table from "../../UI/Table";
 import toLocalDateShort from "../../Utils/toLocalDateShort";
 import { toPersianNumbersWithComma } from "../../Utils/toPersianNumbers";
 import truncateText from "../../Utils/truncateText";
+import { HiOutlineTrash } from "react-icons/hi";
+import { useState } from "react";
+import Modal from "../../UI/Modal";
 
 function ProjectRow({ project, index }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
   return (
     <Table.Row>
       <td>{index + 1}</td>
@@ -36,7 +43,37 @@ function ProjectRow({ project, index }) {
         )}
       </td>
 
-      <td>...</td>
+      <td>
+        <div className="flex items-center justify-center gap-x-2 text-right">
+          <>
+            <button onClick={() => setIsEditOpen(true)}>
+              <TbPencilMinus className="h-5 w-5 text-primary-900" />
+            </button>
+
+            <Modal
+              open={isEditOpen}
+              onClose={() => setIsEditOpen(false)}
+              title={`ویرایش ${project.title}`}
+            >
+              یک متن تستی ...
+            </Modal>
+          </>
+
+          <>
+            <button onClick={() => setIsDeleteOpen(true)}>
+              <HiOutlineTrash className="h-5 w-5 text-error" />
+            </button>
+
+            <Modal
+              open={isDeleteOpen}
+              onClose={() => setIsDeleteOpen(false)}
+              title={`حذف ${project.title}`}
+            >
+              یک متن تستی ...
+            </Modal>
+          </>
+        </div>
+      </td>
     </Table.Row>
   );
 }
