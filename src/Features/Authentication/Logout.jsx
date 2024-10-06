8d2bc4afc9176914cda2463eb1fbe681d6cd0c52
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-10-06 18:55:52 +0330

    Develope "Logout" feature

--- src/Features/Authentication/Logout.jsx
+++ src/Features/Authentication/Logout.jsx
@@ -1,8 +1,11 @@
 import { HiOutlineArrowRightOnRectangle } from "react-icons/hi2";
+import useLogout from "./useLogout";
 
 function Logout() {
+  const { logout } = useLogout();
+  
   return (
-    <button>
+    <button onClick={logout}>
       <HiOutlineArrowRightOnRectangle className="h-5 w-5 text-secondary-600 hover:text-error" />
     </button>
   );
