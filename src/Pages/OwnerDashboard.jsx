a11cbec5d0ae5c9bfb0506fb812d251ea3c32992
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-10-07 21:48:53 +0330

    Develope "OwnerDashboard" component

--- src/Pages/OwnerDashboard.jsx
+++ src/Pages/OwnerDashboard.jsx
@@ -1,6 +1,19 @@
+import Stats from "../Features/Owner/Stats";
+import useOwnerProjects from "../Features/Projects/useOwnerProjects";
+import DashboardHeader from "../UI/DashboardHeader";
+import Loader from "../UI/Loader";
+
 function OwnerDashboard() {
+  const { isLoading, projects } = useOwnerProjects();
+
+  if (isLoading) return <Loader />;
+
   return (
-    <div>Owner Dashboard Panel</div>
-  )
+    <div>
+      <DashboardHeader />
+
+      <Stats projects={projects} />
+    </div>
+  );
 }
-export default OwnerDashboard
+export default OwnerDashboard;
