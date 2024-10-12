8fb9b1a67c7bdf1d51dbf567108581e538231192
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-10-12 22:48:46 +0330

    Develope "Filters" feature

--- src/Features/Freelancer/Projects/ProjectsHeader.jsx
+++ src/Features/Freelancer/Projects/ProjectsHeader.jsx
@@ -1,9 +1,59 @@
+import useCategories from "../../../Hooks/useCategories";
+import FilterButton from "../../../UI/FilterButton";
+import FilterDropDown from "../../../UI/FilterDropDown";
+
 function ProjectsHeader() {
+  const { transformedCategories } = useCategories();
+
   return (
     <div className="mb-8 flex items-center justify-between px-4">
       <h1 className="text-xl font-black text-secondary-700">لیست پروژه ها</h1>
 
-      <div>فیلتر ها ...</div>
+      <div className="flex flex-row-reverse items-center gap-x-4">
+        <FilterDropDown
+          filterField="category"
+          options={[
+            {
+              label: "همه دسته بندی ها",
+              value: "ALL",
+            },
+            ...transformedCategories,
+          ]}
+        />
+
+        <FilterDropDown
+          filterField="sort"
+          options={[
+            {
+              label: "جدید ترین",
+              value: "latest",
+            },
+            {
+              label: "قدیمی ترین",
+              value: "earliest",
+            },
+          ]}
+        />
+
+        <FilterButton
+          filterField="status"
+          title="وضعیت :"
+          options={[
+            {
+              label: "همه",
+              value: "ALL",
+            },
+            {
+              label: "باز",
+              value: "OPEN",
+            },
+            {
+              label: "بسته",
+              value: "CLOSED",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 }
