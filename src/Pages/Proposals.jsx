7191a841dbc85867b65aa13e06aeaeb11b2fc714
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-10-15 20:36:04 +0330

    Develope "Proposals" route

--- src/Pages/Proposals.jsx
+++ src/Pages/Proposals.jsx
@@ -1,6 +1,13 @@
+import ProposalsHeader from "../Features/Admin/Proposal/ProposalsHeader";
+import ProposalsTable from "../Features/Admin/Proposal/ProposalsTable";
+
 function Proposals() {
   return (
-    <div>Proposals</div>
-  )
+    <div>
+      <ProposalsHeader />
+
+      <ProposalsTable />
+    </div>
+  );
 }
-export default Proposals
+export default Proposals;
