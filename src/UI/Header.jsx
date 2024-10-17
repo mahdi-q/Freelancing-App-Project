c32cdff29f1561dcf04b265f04671f8e77bde06f
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-10-17 21:01:19 +0330

    Adding logo to website

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import UserAvatar from "../Features/Authentication/UserAvatar";
 import useUser from "../Features/Authentication/useUser";
 import HeaderMenu from "./HeaderMenu";
@@ -6,11 +7,17 @@ function Header() {
   const { isLoading } = useUser();
 
   return (
-    <div className="border-b border-secondary-300 bg-secondary-0 px-8 py-4">
+    <div className="border-b border-secondary-300 bg-secondary-0 px-8 py-2">
       <div
         className={`container flex items-center justify-between px-0 xl:max-w-screen-lg ${isLoading ? "opacity-30 blur-sm" : ""}`}
       >
-        <UserAvatar />
+        <div className="flex items-center gap-x-4">
+          <Link to="/">
+            <img className="h-14 w-14" src="/logo.svg" alt="Logo" />
+          </Link>
+
+          <UserAvatar />
+        </div>
 
         <HeaderMenu />
       </div>
